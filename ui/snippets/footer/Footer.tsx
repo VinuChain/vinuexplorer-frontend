0facae9446264c243e6c06777707972896dfdb55
Elemont <elemontfx@outlook.com>
2026-04-13 11:10:38 +1000

    feat: trigger backend redeploy after image push; fix footer link to vinuexplorer-frontend

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -23,8 +23,8 @@ import getApiVersionUrl from './utils/getApiVersionUrl';
 
 const MAX_LINKS_COLUMNS = 4;
 
-const FRONT_VERSION_URL = `https://github.com/VinuChain/vinuexplorer-backend/tree/${config.UI.footer.frontendVersion}`;
-const FRONT_COMMIT_URL = `https://github.com/VinuChain/vinuexplorer-backend/commit/${config.UI.footer.frontendCommit}`;
+const FRONT_VERSION_URL = `https://github.com/VinuChain/vinuexplorer-frontend/tree/${config.UI.footer.frontendVersion}`;
+const FRONT_COMMIT_URL = `https://github.com/VinuChain/vinuexplorer-frontend/commit/${config.UI.footer.frontendCommit}`;
 
 const Footer = () => {
 
